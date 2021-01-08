e55d1442ab0cc9c6a198d14f18a56a58abea7bed
lameothirtiia <76797192+lameothirtiia@users.noreply.github.com>
2021-01-08 16:20:31 +0900

    Update multiclass_classification.py

--- multiclass_classification.py
+++ multiclass_classification.py
@@ -68,3 +68,23 @@ plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
+
+# モデルの訓練をやり直す
+model = models.Sequential()
+model.add(layers.Dense(64, activation'relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(partial_x_train,
+          partial_y_train,
+          epochs=8,
+          batch_size=512,
+          validation_data=(x_val, y_val))
+results = model.evaluate(x_test, one_hot_test_labels)
+
+results
+
