52ecdb95625a3d4dd0134637fede98bf8263ad9b
lameothirtiia <76797192+lameothirtiia@users.noreply.github.com>
2021-01-08 14:33:53 +0900

    Create multiclass_classification.py

---
+++ multiclass_classification.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import reuters
+from keras.utils.np_utils import to_categorical
+from keras import models
+from keras import layers
+
+# Reuters データセットを読み込む
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+# データのエンコーディング
+def vectorize_sequences(sequences, dimension=10000):
+  results = np.zeros((len(sequences), dimension))
+  for i, sequence in enumerate(sequences):
+    results[i, sequence] = 1.
+  return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
+
+# モデルの定義
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+# モデルのコンパイル
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 検証データセットの設定
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+# モデルの訓練
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+
+epochs = range(1, len(loss) + 1)
+
+# 訓練データと検証データでの損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 訓練データと検証データでの正解率をプロット
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
