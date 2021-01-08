c392cdf0370cdba59891fabf3340ade76b848cd2
lameothirtiia <76797192+lameothirtiia@users.noreply.github.com>
2021-01-08 16:13:03 +0900

    Update binary_classification.py

--- binary_classification.py
+++ binary_classification.py
@@ -67,3 +67,21 @@ plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
+
+# モデルの訓練をやり直す
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train,
+          y_train,
+          epochs=4,
+          batch_size=512)
+results = model.evaluate(x_test, y_test)
+
+results
